8f5f02f1b0c767a66f0b009b588f0a6a71b50d91
Steev <mechintos@gmail.com>
2021-04-29 12:02:38 +0200

    Képesitó vizsga JurassicPark feladat kész JdbcTemplate használatával

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/jurassic/JurassicPark.java
@@ -1,5 +1,7 @@
 package hu.nive.ujratervezes.kepesitovizsga.jurassic;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -10,28 +12,37 @@ import java.util.List;
 
 public class JurassicPark {
 
-    DataSource dataSource;
+//    DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
 
     public JurassicPark(DataSource dataSource) {
-        this.dataSource = dataSource;
+       // this.dataSource = dataSource;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     public List<String> checkOverpopulation() {
-        try (
-                Connection conn = dataSource.getConnection();
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery("SELECT breed FROM dinosaur WHERE actual > expected ORDER BY breed ASC ")
-        ) {
-            List<String> overpopulatedDinos = new ArrayList<>();
-
-            while (rs.next()) {
-                String name = rs.getString("breed");
-                overpopulatedDinos.add(name);
-            }
-
-            return overpopulatedDinos;
-        } catch (SQLException se) {
-            throw new IllegalStateException("Cannot select employees", se);
-        }
+        return jdbcTemplate.query("SELECT breed FROM dinosaur WHERE actual > expected ORDER BY breed ASC",
+                (resultSet, i) -> resultSet.getString("breed")
+        );
     }
+
+//    public List<String> checkOverpopulation() {
+//        try (
+//                Connection conn = dataSource.getConnection();
+//                Statement stmt = conn.createStatement();
+//                ResultSet rs = stmt.executeQuery("SELECT breed FROM dinosaur WHERE actual > expected ORDER BY breed ASC")
+//        ) {
+//            List<String> overpopulatedDinos = new ArrayList<>();
+//
+//            while (rs.next()) {
+//                String name = rs.getString("breed");
+//                overpopulatedDinos.add(name);
+//            }
+//
+//            return overpopulatedDinos;
+//        } catch (SQLException se) {
+//            throw new IllegalStateException("Cannot select dinosaur", se);
+//        }
+//    }
+
 }
